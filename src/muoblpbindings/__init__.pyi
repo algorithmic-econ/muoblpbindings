32c7a1d824975966cd2dfdc966733cc4df7fdd2c
jasieksz <jasiek.sznajd@gmail.com>
2025-07-06 21:36:12 +0200

    mes_utils with precomputed winners and budget implementation

--- src/muoblpbindings/__init__.pyi
+++ src/muoblpbindings/__init__.pyi
@@ -5,7 +5,7 @@ def equal_shares_utils(voters: list[str],
                  total_utility: dict[str, int],
                  total_budget: float) -> list[str]:
     """
-    Equal shares with utilities implementation
+    Equal shares baseline
     """
 
 def equal_shares_add1(voters: list[str],
@@ -15,5 +15,17 @@ def equal_shares_add1(voters: list[str],
                  total_utility: dict[str, int],
                  total_budget: float) -> list[str]:
     """
-    Equal shares with utilities implementation
+    Equal shares Add1
+    """
+
+def equal_shares_utils_precomputed(voters: list[str],
+                 projects: list[str],
+                 cost: dict[str, float],
+                 approvals_utilities: dict[str, list[tuple[str, int]]],
+                 total_utility: dict[str, int],
+                 total_budget: float,
+                 budget: dict[str, float],
+                 winners: list[str]) -> list[str]:
+    """
+    Equal shares precomputed winners implementation
     """
